33bc655db3407c27b3aed4e85eae20f29a51e14c
shahidur24 <shahidur2424@gmail.com>
2021-06-26 23:26:45 +0600

    Pdf generate Automation

--- src/main/java/com/exam/screenshot/GeneratePdf.java
+++ src/main/java/com/exam/screenshot/GeneratePdf.java
@@ -1,33 +1,39 @@
-//package com.exam.screenshot;
-//
-//import com.exam.base.TestBase;
-//import com.itextpdf.text.Document;
-//import com.itextpdf.text.Image;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.io.FileWriter;
-//
-//public class GeneratePdf extends TestBase {
-//    public static void main(String[] args) {
-//       chromeLaunch();
-//       openURL("");
-//       createPDF();
-//
-//    }
-//    public static void createPDF(){
-//        byte[] input = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-//        Document doc = new Document();
-//        String s= System.getProperty("");
-//                FileOutputStream f= new FileOutputStream("");
-//                FileWriter f2=
-//            writer.]
-//        doc.open();
-//        Image img =
-//                doc
-//                doc
-//
-//    }
-//}
+package com.exam.screenshot;
+
+import com.exam.base.TestBase;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GeneratePdf extends TestBase {
+    public static void main(String[] args) throws IOException, DocumentException {
+        chromeLaunch();
+        openURL("https://demo.opencart.com/");
+        createPDF();
+
+    }
+
+    public static void createPDF() throws IOException, DocumentException {
+        //take screenshot and save as byte[]
+        byte[] input = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        Document doc = new Document();
+        String output = System.getProperty("user.dir") + "//src//main/PDF/New.pdf";
+        FileOutputStream fs = new FileOutputStream(output);
+        PdfWriter writer = PdfWriter.getInstance(doc, fs);
+        writer.open();
+        doc.open();
+        Image img = Image.getInstance(input);
+        img.scaleToFit(PageSize.A4.getWidth() / 2, PageSize.A4.getHeight() / 2);
+        doc.add(img);
+        doc.add(new Paragraph(""));
+        doc.close();
+        writer.close();
+
+    }
+}
