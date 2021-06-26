2555059caf229c7d12a57d62eaff3c9475dda59d
shahidur24 <shahidur2424@gmail.com>
2021-06-26 23:13:10 +0600

    Page Screenshot Automation

--- src/main/java/com/exam/screenshot/PageScreenShots.java
+++ src/main/java/com/exam/screenshot/PageScreenShots.java
@@ -1,25 +1,25 @@
-//package com.exam.screenshot;
-//
-//import com.exam.base.TestBase;
-//import org.apache.commons.io.FileUtils;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class PageScreenShots extends TestBase {
-//    public static void main(String[] args) throws IOException {
-//        chromeLaunch();
-//        openURL("https://google.com");
-//        screenshot();
-//        closeBrowser();
-//    }
-//
-//    public static void screenshot() throws IOException {
-//        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//        //FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "src//main//resources//screenshots//" + "name" + ".png"));
-//        FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "src//main//resources//screenshots//" + "name" + ".png"));
-//    System.out.println("ok");
-//    }
-//}
+package com.exam.screenshot;
+
+import com.exam.base.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PageScreenShots extends TestBase {
+    public static void main(String[] args) throws IOException {
+        chromeLaunch();
+        openURL("https://google.com");
+        screenshot("Page_Image");
+        closeBrowser();
+    }
+
+    public static void screenshot(String name) throws IOException {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        //FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "src//main//resources//screenshots//" + "name" + ".png"));
+        FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "//src//main//resources//screenshots//" + name + ".png"));
+    System.out.println("ok");
+    }
+}
